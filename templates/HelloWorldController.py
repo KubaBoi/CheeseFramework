4a3c1b61e7e590bdb66ec644e17010b56041116c
KubaBoi <jakubanderle@outlook.cz>
2022-04-06 03:06:12 +0200

    debug

--- templates/HelloWorldController.py
+++ templates/HelloWorldController.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from cheese.resourceManager import ResMan
-from cheese.modules.cheeseController import CheeseController
+from cheese.modules.cheeseController import CheeseController as cc
 from cheese.ErrorCodes import Error
 
 #@controller /hello
-class HelloWorldController(CheeseController):
+class HelloWorldController(cc):
 
     #@get /world
     @staticmethod
     def helloWorld(server, path, auth):
-        response = CheeseController.createResponse({"HELLO WORLD": "Cheese is working :) hurrayyy"}, 200)
-        CheeseController.sendResponse(server, response)
+        response = cc.createResponse({"HELLO WORLD": "Cheese is working :) hurrayyy"}, 200)
+        cc.sendResponse(server, response)
