372aad46525c29156034a88268d1a4ed2f7e8f7f
Jakub Anderle <jakub.anderle@aimtecglobal.com>
2022-04-07 16:35:00 +0200

    debug

--- public/scripts/tableBuilder.js
+++ public/scripts/tableBuilder.js
@@ -15,7 +15,6 @@ function addRow(table, cells, rowAttributes=[]) {
 function addHeader(table, cells, rowAttributes=[]) {
     var row = createElement("tr", table, "", rowAttributes);
     for (let i = 0; i < cells.length; i++) {
-        console.log(cells[i]);
         if (!cells[i].attributes)
             cells[i].attributes = [];
         createElement("th", row, cells[i].text, cells[i].attributes);
