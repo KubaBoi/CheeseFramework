9226590608f0b53e13c093fe33f4e715a27d2b17
Jakub Anderle <jakub.anderle@aimtecglobal.com>
2022-04-07 17:04:09 +0200

    debug

--- public/scripts/autoComplete.js
+++ public/scripts/autoComplete.js
@@ -11,31 +11,29 @@ function autocomplete(inp, arr) {
         if (!val) { return false; }
         currentFocus = -1;
         /*create a DIV element that will contain the items (values):*/
-        a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
-        /*append the DIV element as a child of the autocomplete container:*/
-        this.parentNode.appendChild(a);
+        a = createElement("div", this.parentNode, "", [
+          {"name": "id", "value": this.id + "autocomplete-list"},
+          {"name": "class", "value": "autocomplete-items"}
+        ]);
+
         /*for each item in the array...*/
         for (i = 0; i < arr.length; i++) {
           /*check if the item starts with the same letters as the text field value:*/
           if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
             /*create a DIV element for each matching element:*/
-            b = document.createElement("DIV");
-            /*make the matching letters bold:*/
-            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-            b.innerHTML += arr[i].substr(val.length);
-            /*insert a input field that will hold the current array item's value:*/
-            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            b = createElement("div", a, 
+              "<strong>" + arr[i].substr(0, val.length) + "</strong>" +
+              arr[i].substr(val.length) +
+              "<input type='hidden' value='" + arr[i] + "'>");
+
             /*execute a function when someone clicks on the item value (DIV element):*/
-                b.addEventListener("click", function(e) {
+            b.addEventListener("click", function(e) {
                 /*insert the value for the autocomplete text field:*/
                 inp.value = this.getElementsByTagName("input")[0].value;
                 /*close the list of autocompleted values,
                 (or any other open lists of autocompleted values:*/
                 closeAllLists();
             });
-            a.appendChild(b);
           }
         }
     });
