8fb841015e61c592fc18d63427babf7a98e67b00
Jakub Anderle <jakub.anderle@aimtecglobal.com>
2022-04-07 16:32:25 +0200

    debug

--- public/scripts/tableBuilder.js
+++ public/scripts/tableBuilder.js
@@ -7,7 +7,7 @@ function addRow(table, cells, rowAttributes=[]) {
     var row = createElement("tr", table, "", rowAttributes);
     for (let i = 0; i < cells.length; i++) {
         if (!cells[i].attributes)
-            cells[i].attributes = []
+            cells[i].attributes = [];
         createElement("td", row, cells[i].text, cells[i].attributes);
     }
 }
@@ -15,8 +15,9 @@ function addRow(table, cells, rowAttributes=[]) {
 function addHeader(table, cells, rowAttributes=[]) {
     var row = createElement("tr", table, "", rowAttributes);
     for (let i = 0; i < cells.length; i++) {
+        console.log(cells[i]);
         if (!cells[i].attributes)
-            cells[i].attributes = []
+            cells[i].attributes = [];
         createElement("th", row, cells[i].text, cells[i].attributes);
     }
 }
