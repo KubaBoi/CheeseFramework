c3be1928c2efb0b8d21b782f8bca7ba2732f21ec
KubaBoi <jakubanderle@outlook.cz>
2022-04-07 19:05:54 +0200

    debug

--- cheeserScripts/releaseDate.py
+++ cheeserScripts/releaseDate.py
@@ -3,12 +3,13 @@ import json
 from datetime import datetime, timedelta, timezone
 
 propFile = os.path.abspath(os.path.join(os.path.dirname( __file__ ), "..", "cheese", "cheeseproperties.json"))
-
+print(propFile)
 with open(propFile, "r") as f:
     properties = json.loads(f.read())
 
 now = datetime.now(timezone.utc)
 properties["release"] = now.strftime("%y.%m.%d.%H")
+print(properties["release"])
 
 with open(propFile, "w") as f:
     f.write(json.dumps(properties))
