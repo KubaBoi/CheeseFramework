bcbbc3f072c29ba90118f9d4fb4d1a0f7fb0c1ad
KubaBoi <jakubanderle@outlook.cz>
2022-04-07 22:03:13 +0200

    debug

--- .github/releaseDate.py
+++ .github/releaseDate.py
@@ -2,7 +2,7 @@ import os
 import json
 from datetime import datetime, timedelta, timezone
 
-prepFile = os.path.abspath(os.path.join(__file__, "..", "cheese", "cheeseproperties.json"))
+prepFile = os.path.abspath(os.path.join(__file__, "..", ".." "cheese", "cheeseproperties.json"))
 
 now = datetime.now(timezone.utc) + timedelta(hours=2)
 releaseDate = now.strftime("%y.%m.%d.%H.%M")
