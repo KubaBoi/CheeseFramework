8bfad43fd1d394fdf4b96bf2e372779503e384ec
KubaBoi <jakubanderle@outlook.cz>
2022-04-20 20:36:47 +0200

    debug

--- cheeserScripts/projectGenerator.py
+++ cheeserScripts/projectGenerator.py
@@ -33,14 +33,16 @@ class ProjectGenerator:
 
     def generateFile(self, template, copy):
         if (self.debug):
-            print(f"Generating {copy}")
-        if (os.path.exists(copy)):
-            if (self.debug):
-                print(f"{copy} already exists. Skipping")
-        else:
-            with open(f"{Path(__file__).parent.parent}/templates/{template}", "r") as r:
-                with open(copy, "w") as w:
-                    w.write(r.read())
+            print(f"Generating {copy[0]}")
+        for file in copy:
+            if (os.path.exists(file)):
+                if (self.debug):
+                    print(f"{file} already exists. Skipping")
+                return
+
+        with open(f"{Path(__file__).parent.parent}/templates/{template}", "r") as r:
+            with open(copy[0], "w") as w:
+                w.write(r.read())
 
     def generateRoot(self):
         self.generateFolder(ResMan.root())
@@ -60,17 +62,17 @@ class ProjectGenerator:
     def generateFiles(self):
         if (self.debug):
             print("=====Generating Files=====")
-        if (not os.path.exists(f"{ResMan.src()}/{self.pname}.py")):
-            self.generateFile("HelloWorldController.py", f"{ResMan.pythonSrc()}/controllers/HelloWorldController.py")
+        if (not os.path.exists(f"{ResMan.src()}/{self.pname}.py") and not os.path.exists(f"{ResMan.src()}/{self.pname}.pyw")):
+            self.generateFile("HelloWorldController.py", [f"{ResMan.pythonSrc()}/controllers/HelloWorldController.py"])
 
-        self.generateFile("mainTemplate.py", f"{ResMan.src()}/{self.pname}.py")
-        self.generateFile("appSettings.json", f"{ResMan.root()}/appSettings.json")
-        self.generateFile("adminSettings.json", f"{ResMan.root()}/adminSettings.json")
-        self.generateFile("index.html", f"{ResMan.web()}/index.html")
-        self.generateFile("api.html", f"{ResMan.web()}/api.html")
-        self.generateFile("error404.html", f"{ResMan.error()}/error404.html")
-        self.generateFile("authorization.py", f"{ResMan.pythonSrc()}/authorization.py")
-        self.generateFile(".gitignore", f"{ResMan.root()}/.gitignore")
+        self.generateFile("mainTemplate.py", [f"{ResMan.src()}/{self.pname}.py", f"{ResMan.src()}/{self.pname}.pyw"])
+        self.generateFile("appSettings.json", [f"{ResMan.root()}/appSettings.json"])
+        self.generateFile("adminSettings.json", [f"{ResMan.root()}/adminSettings.json"])
+        self.generateFile("index.html", [f"{ResMan.web()}/index.html"])
+        self.generateFile("api.html", [f"{ResMan.web()}/api.html"])
+        self.generateFile("error404.html", [f"{ResMan.error()}/error404.html"])
+        self.generateFile("authorization.py", [f"{ResMan.pythonSrc()}/authorization.py"])
+        self.generateFile(".gitignore", [f"{ResMan.root()}/.gitignore"])
 
     def copyFramework(self):
         if (self.debug):
