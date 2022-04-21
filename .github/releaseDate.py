49006934cdcded2841b9de685c5b2e122f3d74d4
Jakub Anderle <jakub.anderle@aimtecglobal.com>
2022-04-21 15:47:41 +0200

    debug

--- .github/releaseDate.py
+++ .github/releaseDate.py
@@ -20,7 +20,7 @@ readmeFile = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "READ
 with open(readmeFile, "w") as f:
     f.write(
         f"# Cheese Framework\n\n" +
-        "[![Release Build](https://github.com/KubaBoi/CheeseFramework/actions/workflows/realeaseDate.yml/badge.svg)](https://github.com/KubaBoi/CheeseFramework/actions/workflows/realeaseDate.yml)\n\n" +
+        f"[![Release v({releaseDate})](https://github.com/KubaBoi/CheeseFramework/actions/workflows/realeaseDate.yml/badge.svg)](https://github.com/KubaBoi/CheeseFramework/actions/workflows/realeaseDate.yml)\n\n" +
         f"### Release v({releaseDate})\n\n" +
         "## Documentation\n\nhttps://kubaboi.github.io/CheeseFramework/"
     )
