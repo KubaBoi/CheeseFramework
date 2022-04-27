0060404f4f2f4f78c4cdd3113932c7dc517b6a00
KubaBoi <jakubanderle@outlook.cz>
2022-04-28 00:10:19 +0200

    test upload

---
+++ .github/setCredentials.py
@@ -0,0 +1,26 @@
+import os
+import json
+
+props = os.path.abspath(os.path.join(os.path.dirname( __file__ ), "..", "uploadProperites.json"))
+
+if (not os.path.exists(props)):
+    print("Not building - missing credentials")
+    raise Exception
+
+with open(props, "r") as f:
+    data = json.loads(f.read())
+
+os.remove(props)
+
+userName = props["name"]
+password = props["pass"]
+
+with open("~/.pypirc", "r") as f:
+    data = f.read()
+
+with open("~/.pypirc", "a") as f:
+    f.write(f"username = {userName}")
+    f.write(f"password = {password}")
+
+
+
