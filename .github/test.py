0866f1890d678a2c906b0fbfef6d69e57191e456
KubaBoi <jakubanderle@outlook.cz>
2022-04-27 22:52:13 +0200

    test

--- .github/test.py
+++ .github/test.py
@@ -3,3 +3,7 @@ import os
 prepFile = os.path.abspath(os.path.join(os.path.dirname( __file__ ), "..", ".."))
 
 print(prepFile)
+
+for root, dirs, files in os.walk(prepFile):
+    for dir in dirs:
+        print(dir)
