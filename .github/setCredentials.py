76695f57ba49ae0c02791c87aac2b0ca9662ceb6
KubaBoi <jakubanderle@outlook.cz>
2022-04-28 00:16:22 +0200

    test5

--- .github/setCredentials.py
+++ .github/setCredentials.py
@@ -2,6 +2,7 @@ import os
 import json
 
 props = os.path.abspath(os.path.join(os.path.dirname( __file__ ), "..", "cr.json"))
+pypirc = "~/.pypirc"
 
 if (not os.path.exists(props)):
     print("Not building - missing credentials")
@@ -15,10 +16,15 @@ os.remove(props)
 userName = data["name"]
 password = data["pass"]
 
-with open("/.pypirc", "r") as f:
+if (not os.path.exists(pypirc)):
+    with open(pypirc, "w") as f:
+        f.write("[testpypi]\n")
+        f.write("repository = https://test.pypi.org/legacy/")
+
+with open("~/.pypirc", "r") as f:
     data = f.read()
 
-with open("/.pypirc", "a") as f:
+with open("~/.pypirc", "a") as f:
     f.write(f"username = {userName}")
     f.write(f"password = {password}")
 
