77bf591960b60cc0dfc260386f92530e2286a277
GitHub Action <action@github.com>
2022-04-27 21:42:08 +0000

    automatic build for pip

--- src/Cheese/variables.py
+++ src/Cheese/variables.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 #cheese
 
 class Variables:
     release = "RELEASE"
     documentation = "https://kubaboi.github.io/CheeseFramework"
-=======
-#cheese
-
-class Variables:
-    release = "RELEASE"
-    documentation = "https://kubaboi.github.io/CheeseFramework"
->>>>>>> a53dfb8efb2803df0b3fffb8e3f0f85d8b0ccac0
