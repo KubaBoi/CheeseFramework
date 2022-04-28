7eb3c7a8ed845914376dcad54e15baa2e0c7e6f6
Jakub Anderle <jakub.anderle@aimtecglobal.com>
2022-04-28 11:36:52 +0200

    debug

--- cheese/metadata.py
+++ cheese/metadata.py
@@ -27,11 +27,11 @@ class Metadata:
             if (endpoint["MAIN_ENDPOINT"] == endpoints[0]):
                 for method in endpoint["METHODS"]:
                     if (method["ENDPOINT"] == endpoints[1] and method["TYPE"] == httpMethod):
-                        return Metadata.getMethod(endpoint, method)
+                        return Metadata.getHttpMethod(endpoint, method)
         return False 
 
     @staticmethod
-    def getMethod(endpoint, method):
+    def getHttpMethod(endpoint, method):
         path = endpoint["FILE"].split("/")
         a = __import__(path[0])
         for i in range(1, len(path)):
