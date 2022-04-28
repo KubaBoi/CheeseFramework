0c4273a09bbdca691f0df7bedd0d1367997fcaac
KubaBoi <jakubanderle@outlook.cz>
2022-04-28 05:53:28 +0200

    debug

--- .github/releaseDate.py
+++ .github/releaseDate.py
@@ -7,13 +7,8 @@ prepFile = os.path.abspath(os.path.join(os.path.dirname( __file__ ), "..", "chee
 now = datetime.now(timezone.utc) + timedelta(hours=2)
 releaseDate = now.strftime("%y.%m.%d.%H.%M")
 
-with open(prepFile, "r") as f:
-    data = json.loads(f.read())
-
-data["release"] = releaseDate
-
 with open(prepFile, "w") as f:
-    f.write(json.dumps(data))
+    f.write(json.dumps({"release": releaseDate}))
 
 readmeFile = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "README.md"))
 
