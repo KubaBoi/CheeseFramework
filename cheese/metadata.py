0c6a9ffd18261b17f52da4fc2314dd200670bc08
KubaBoi <jakubanderle@outlook.cz>
2022-04-30 16:17:04 +0200

    new compiler

--- cheese/metadata.py
+++ cheese/metadata.py
@@ -3,8 +3,8 @@
 import os
 import json
 
-from cheese.resourceManager import ResMan
-from cheese.Logger import Logger
+from Cheese.resourceManager import ResMan
+from Cheese.Logger import Logger
 
 class Metadata:
     
@@ -12,74 +12,80 @@ class Metadata:
     models = None
     contr = None
 
+    getEndpoints = {}
+    postEndpoints = {}
+
     @staticmethod
     def loadMedatada():
         try:
-            with open(os.path.join(ResMan.metadata(), "contMetadata.json"), "r") as f:
-                Metadata.contr = json.loads(f.read())["CONTROLLERS"]
-
-            with open(os.path.join(ResMan.metadata(), "repMetadata.json"), "r") as f:
+            with open(ResMan.metadata(), "r") as f:
                 data = json.loads(f.read())
-                Metadata.repos = data["REPOSITORIES"]
-                Metadata.models = data["MODELS"]
+
+            Metadata.repos = data["REPOSITORIES"]
+            Metadata.models = data["MODELS"]
+            Metadata.contr = data["CONTROLLERS"]
+
+            Metadata.prepareEndpoints()
         except Exception as e:
             Logger.fail("Error while loading metadata", e, False, False)
-            Logger.warning("Didn't you forgot to build application?", e, False, False)
-            Logger.warning("Build will be triggered when application is in debug mode", e, False, False)
+            Logger.warning("Didn't you forgot to build application?", False, False)
+            Logger.warning("Build will be triggered when application is in debug mode", False, False)
             raise SystemError("Error while loading metadata", e)
 
     @staticmethod
-    def findMethod(endpoints, httpMethod):
-        for endpoint in Metadata.contr:
-            if (endpoint["MAIN_ENDPOINT"] == endpoints[0]):
-                for method in endpoint["METHODS"]:
-                    if (method["ENDPOINT"] == endpoints[1] and method["TYPE"] == httpMethod):
-                        return Metadata.getHttpMethod(endpoint, method)
+    def prepareEndpoints():
+        for key in Metadata.contr.keys():
+            controller = Metadata.contr[key]
+            mainEndpoint = controller["CONTROLLER"]
+
+            for methodKey in controller["METHODS"].keys():
+                method = controller["METHODS"][methodKey]
+                for endpointKey in method.keys():
+                    eKey = mainEndpoint + method[endpointKey]
+
+                    path = controller["FILE"].split("/")
+                    a = __import__(path[0])
+                    for i in range(1, len(path)):
+                        a = getattr(a, path[i])
+                    
+                    methodObj = getattr(a, methodKey)
+
+                    if (endpointKey == "GET"):
+                        Metadata.getEndpoints[eKey] = methodObj
+                    elif (endpointKey == "POST"):
+                        Metadata.postEndpoints[eKey] = methodObj
+
+    @staticmethod
+    def findMethod(endpoint, httpMethod):
+        if (httpMethod.upper() == "GET"):
+            if (endpoint in Metadata.getEndpoints):
+                return Metadata.getEndpoints[endpoint]
+
         return False 
 
     @staticmethod
-    def getHttpMethod(endpoint, method):
-        path = endpoint["FILE"].split("/")
-        a = __import__(path[0])
-        for i in range(1, len(path)):
-            a = getattr(a, path[i])
-        
-        return getattr(a, method["METHOD"])
-
-    @staticmethod
     def getRepository(userRepository):
-        for repo in Metadata.repos:
-            if (repo["FILE"] == userRepository):
-                return repo
+        for repo in Metadata.repos.keys():
+            repos = Metadata.repos[repo]
+            for repoKey in repos.keys():
+                if (repos[repoKey]["FILE"] == userRepository):
+                    return repo
         raise SyntaxError(f"Repository {userRepository} was not found")
 
     @staticmethod
     def getRepositoryFromClass(userRepository):
-        for repo in Metadata.repos:
-            if (repo["CLASS"] == userRepository):
-                return repo
+        if (userRepository in Metadata.repos.keys()):
+            return Metadata.repos[userRepository]
         raise SyntaxError(f"Repository {userRepository} was not found")
 
 
     @staticmethod
     def getMethod(repository, methodName):
-        for method in repository["METHODS"]:
-            if (method["METHOD"] == methodName):
-                return method
+        if (methodName in repository["METHODS"].keys()):
+            return repository["METHODS"][methodName]
         raise SyntaxError(f"Method {methodName} in {repository['FILE']} was not found")
 
     @staticmethod
     def getModel(repository):
-        for model in Metadata.models:
-            if (model["CLASS"] == repository["MODEL"]):
-                return model
-        raise SyntaxError(f"Model {repository['MODEL']} for {repository['FILE']} was not found")
+        return repository["DBMODEL"]
         
-    @staticmethod
-    def getModelObject(model):
-        path = model["FILE"].split("/")
-        a = __import__(path[0])
-        for i in range(1, len(path)):
-            a = getattr(a, path[i])
-        
-        return a
--- cheeserScripts/builders/repositoriesBuilder.py
+++ cheeserScripts/builders/repositoriesBuilder.py
@@ -12,7 +12,6 @@ class RepositoriesBuilder:
 
     def build(self):
         repositories = []
-        models = []
 
         for root, dirs, files in os.walk(ResMan.src()):
             for file in files:
@@ -22,60 +21,43 @@ class RepositoriesBuilder:
 
                 if (Finder.isSomething(path, "repository")): 
                     repositories.append(path)
-                elif (Finder.isSomething(path, "model")):
-                    models.append(path)
 
         self.parent.doJson(repositories, "REPOSITORIES", 
             ["REPOSITORY", "DBSCHEME", "DBMODEL"],
             [("QUERY", [("RETURN", "raw")]), ("COMMIT", [])])
-        self.parent.doJson(models, "MODELS", [], [])
 
-        #self.preQueries()
-        #self.preCommits()
+        self.preQueries()
+        self.preCommits()
     
 
     # prefabricated query methods
     def preQueries(self):
-        for repo in self.repoJson["REPOSITORIES"]:
-            repo["METHODS"].append(
-                {
-                    "SQL": f"select max(id) from {repo['NAME']};",
-                    "RETURN": "num",
-                    "METHOD": "findNewId",
-                    "TYPE": "query",
-                    "ACCEPTS_MODEL": False
+        for repoKey in self.parent.dictJson["REPOSITORIES"].keys():
+            repo = self.parent.dictJson["REPOSITORIES"][repoKey]
+
+            repo["METHODS"]["findNewId"] = {
+                    "QUERY": f"select max(id) from {repo['REPOSITORY']};",
+                    "RETURN": "num"
                 }
-            )
 
     # prefabricated commit methods 
     def preCommits(self):
-        for repo in self.repoJson["REPOSITORIES"]:
-            name = repo["NAME"]
-            scheme = repo["SCHEME"]
-            repo["METHODS"].append(
-                {
-                    "SQL": f"insert into {name} {scheme} values :obj;",
-                    "RETURN": "",
-                    "METHOD": "save",
-                    "TYPE": "commit",
-                    "ACCEPTS_MODEL": True
+        for repoKey in self.parent.dictJson["REPOSITORIES"].keys():
+            repo = self.parent.dictJson["REPOSITORIES"][repoKey]
+            scheme = repo["DBSCHEME"]
+            name = repo["REPOSITORY"]
+
+            repo["METHODS"]["save"] = {
+                    "COMMIT": f"insert into {name} {scheme} values :obj;",
+                    "RETURN": ""
                 }
-            )
-            repo["METHODS"].append(
-                {
-                    "SQL": f"update {name} set {scheme} = :obj where id=obj.id;",
-                    "RETURN": "",
-                    "METHOD": "update",
-                    "TYPE": "commit",
-                    "ACCEPTS_MODEL": True
+            
+            repo["METHODS"]["update"] = {
+                    "COMMIT": f"update {name} set {scheme} = :obj where id=obj.id;",
+                    "RETURN": ""
                 }
-            )
-            repo["METHODS"].append(
-                {
-                    "SQL": f"delete from {name} where id=model.id;",
-                    "RETURN": "",
-                    "METHOD": "delete",
-                    "TYPE": "commit",
-                    "ACCEPTS_MODEL": True
+
+            repo["METHODS"]["delete"] = {
+                    "COMMIT": f"delete from {name} where id=model.id;",
+                    "RETURN": ""
                 }
-            )
