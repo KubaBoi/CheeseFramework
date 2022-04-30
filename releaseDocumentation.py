e65e6b5dace3795648659ee2d52f6a94c6abc24c
KubaBoi <jakubanderle@outlook.cz>
2022-04-30 19:39:25 +0200

    no action

--- releaseDocumentation.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta, timezone
-
-htmlFile = "./public/docVersion.html"
-
-now = datetime.now(timezone.utc) + timedelta(hours=2)
-releaseDate = now.strftime("%y.%m.%d.%H.%M")
-
-with open(htmlFile, "w") as f:
-    f.write(releaseDate)
