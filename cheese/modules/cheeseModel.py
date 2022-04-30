e8344c53d2261935dc594cfd7bf995937868a28a
KubaBoi <jakubanderle@outlook.cz>
2022-04-30 17:22:04 +0200

    debug

--- cheese/modules/cheeseModel.py
+++ cheese/modules/cheeseModel.py
@@ -1,4 +1,17 @@
 #cheese
 
 class CheeseModel:
-    pass
+    
+    def __init__(self, modelName, scheme):
+        self.modelName = modelName
+        self.scheme = scheme
+
+    def toJson(self):
+        jsn = {}
+        for attr in self.scheme:
+            jsn[attr] = getattr(self, attr)
+        return jsn
+
+    def toModel(self, jsn):
+        for attr in self.scheme:
+            setattr(self, attr, jsn[attr])
