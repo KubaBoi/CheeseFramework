2906d3a6048d9fe7be7e4afc9da0c2d44f367804
GitHub Action <action@github.com>
2022-05-02 16:25:37 +0000

    Test build - 22.05.02.18.25 v(1.1.51)

--- src/Cheese/mockManager.py
+++ src/Cheese/mockManager.py
@@ -9,8 +9,16 @@ class MockManager:
         MockManager.mocks[mock.repoName] = mock
 
     @staticmethod
-    def returnMock(repositoryName, methodName, **kwargs):
+    def returnMock(repositoryName, methodName, kwargs):
+        
         if (repositoryName in MockManager.mocks.keys()):
             mock = MockManager.mocks[repositoryName]
-            print(mock.whenReturns)
-            print(methodName)
+
+            if (methodName in mock.whenReturns.keys()):
+                method = mock.whenReturns[methodName]
+                
+                for ret in method:
+                    if (kwargs == ret["KWARGS"]):
+                        return ret["RESPONSE"]
+        
+        raise SystemError(f"Missing mock {repositoryName}, {methodName}, {kwargs}")
--- src/Cheese/variables.py
+++ src/Cheese/variables.py
@@ -1,5 +1,5 @@
 #cheese
 
 class Variables:
-    release = "22.05.02.18.03"
+    release = "22.05.02.18.25"
     documentation = "https://github.com/KubaBoi/CheeseFramework"
