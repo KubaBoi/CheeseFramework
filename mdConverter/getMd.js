12bc807b6cd8ad45c725732f4dda895103e0e2ec
KubaBoi <jakubanderle@outlook.cz>
2022-05-06 00:23:11 +0200

    denbug

--- mdConverter/getMd.js
+++ mdConverter/getMd.js
@@ -20,7 +20,7 @@ async function source() {
         var response = await callEndpoint("GET", mdUrl);
         if (response.ERROR == null) {
             var code = createElement("textarea", sourceDiv [
-                {"name": "id", "value": "textInput"}
+                {"name": "id", "value": "sourceInputText"}
             ]);
             var lines = response.split("\n");
 
