2c83acd13edc059d5c9127ed40e35d23250c06d8
KubaBoi <jakubanderle@outlook.cz>
2022-05-11 22:08:26 +0200

    debug

--- mdConverter/custom.js
+++ mdConverter/custom.js
@@ -3,9 +3,9 @@ function changeWelcome() {
     var docDiv = document.getElementById("documentationId");
     var str = `
         <h2 id="documentation">Documentation</h2>
-        <br>
-        Yeeey you are here :heart_eyes: <br>That's amazing, thank you. All source codes for this .md to .html converter
-        are <a href="https://github.com/KubaBoi/CheeseFramework/tree/webServices/mdConverter">here</a><br>
+        Yeeey you are here :heart_eyes: 
+        <br>That's amazing, thank you. All source codes for this .md to .html converter
+        are <a href="https://github.com/KubaBoi/CheeseFramework/tree/webServices/mdConverter">here</a>.<br>
     `;
 
     docDiv.innerHTML = rplcReg(str, /:(?<emoji>[a-z0-9_\-\+]+):/g, "$emoji.emoji$");
