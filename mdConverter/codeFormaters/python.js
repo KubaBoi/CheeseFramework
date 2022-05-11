0d0a2e8ee3899299ae2411d5ffbddd0d72bffc56
KubaBoi <jakubanderle@outlook.cz>
2022-05-11 16:51:34 +0200

    new convertor

--- mdConverter/codeFormaters/python.js
+++ mdConverter/codeFormaters/python.js
@@ -6,21 +6,46 @@ function formatPython(str) {
     str = "";
     for (let i = 0; i < lines.length; i++) {
         var line = lines[i];
-        //console.log(line);
+        console.log(line);
+        // from
+        line = rplcReg(line, /(?<!.*)(?<kwFrom>from) (?<from>.+)/, "<span class=keyword>$kwFrom$</span> <span class=class>$from$</span> ", 1)
+        //import
+        line = rplcReg(line, /(?<kwImport>import) (?<import>.*)/,  "<span class=keyword>$kwImport$</span> <span class=class>$import$</span>", 1)
+        // cheese annotations
         line = rplcReg(line, /(?<cheeseAnnot>#@.*;)/, "<span class=cheese_annotation>$cheeseAnnot$</span>", 1);
-
+        // one line comments
         line = rplcReg(line, /^(?!@#)(?<comment>#.*)/, "<span class=comment>$comment$</span>", 1);
-        
-        line = rplcReg(line, /class (?<class>\w+)\((?<parentClass>\w+)\):/, 
-                    "<span class=keywords>class</span> <span class=class>$class$</span>(<span class=class>$parentClass$</span>):", 1);
-        
-        line = rplcReg(line, /def (?<def>\w+)\((?<variables>.*)\):/, 
-                    "<span class=keywords>def</span> <span class=function>$def$</span>(<span class=function_variables>$variables$</span>):", 1);
-        
+        // python annotation
         line = rplcReg(line, /^(?!@#)(?<annot>@.*)/, "<span class=annotation>$annot$</span>", 1);
+        
+        // class
+        line = rplcReg(line, /class (?<class>\w+)\((?<parentClass>\w+)\):/, 
+                    "<span class=keyword>class</span> <span class=class>$class$</span>(<span class=class>$parentClass$</span>):", 1,
+                    {
+                        "parentClass.args": function(vl){return args(vl, "class")}
+                    });
+        // def
+        line = rplcReg(line, /def (?<def>\w+)\((?<variables>.*)\):/, 
+                    "<span class=keyword>def</span> <span class=function>$def$</span>($variables.args$):", 1,
+                    {
+                        "variables.args": function(vl){return args(vl)}
+                    });
 
         str += line + "<br>";
+        console.log(line);
     }
 
     return str;
+}
+
+function args(args, cls="function_variable") {
+    argsArray = args.split(",");
+    newStr = "";
+    for (let i = 0; i < argsArray.length; i++) {
+        newStr += `<span class=${cls}>${argsArray[i].trim()}</span>`
+        if (i < argsArray.length - 1) {
+            newStr += ", ";
+        }
+    }
+    return newStr;
 }
---
+++ mdConverter/mdConverter.js
@@ -0,0 +1,85 @@
+
+var debug = false;
+
+function convert(str) {
+    var mdDiv = document.getElementById("md");
+    var contentsDiv = document.getElementById("contents");
+    clearTable(mdDiv);
+    clearTable(contentsDiv);
+    
+    // hrefs within md document -> [title](#headerId) 
+    str = rplcReg(str, /\[(?<title>.+)\]\((?<href>#.*)\)/g, "<a href=$href.lowerCase$>$title$</a>");
+    
+    // hrefs to another sites -> [title](url)
+    str = rplcReg(str, /\[(?<title>.+)\]\((?<href>.*)\)/g, "<a href=$href$ target=_blank>$title$</a>");
+    
+    // images -> ![title](imgSrc)
+    str = rplcReg(str, /\!\[(?<title>.*)\]\((?<src>.*)\)/g, "<img src=$src$ title=$title$>");
+    
+    // one line codes -> ```code```
+    // no space
+    str = rplcReg(str, /\`{3}(?<code>[a-zA-Z0-9\#\@\&\?\/\:\=]+)\`{3}/g, "<code>$code$</code>");
+    // with space
+    str = rplcReg(str, /\`{3}(?<code>.+)\`{3}/g, "<code>$code$</code>");
+    
+    // check boxes -> [ ] || [x]
+    str = rplcReg(str, /\[(?<checkBox>[ x])\]/g, "$checkBox.checkBox$");
+
+    // contents
+    //str = rplcReg()
+
+    // list -> - something
+    str = rplcReg(str, /^((?<![a-zA-Z0-9])(?<spaces>[ ]*)- )(?<li>.*)/gm, "<li style=margin-left:$spaces.length$%;>$li$</li>");
+
+    //str = str.rplcRegAll(/(?<!\")[ ]*https\:\/\/.*(?=!<\/) /g, urls);
+    str = rplcReg(str, /\`{3}(?<codeType>[a-z]+)/g, "<pre class=language-$codeType$>");
+    str = rplcReg(str, /\`{3}/g, "</pre>");
+
+    mdDiv.innerHTML = str;
+
+    formatCode();
+
+    str = mdDiv.innerHTML;
+
+    // headers -> ## Header
+    str = rplcReg(str, /^(?<hdr>#+) (?<title>.*)/g, "<h$hdr.length$ id=$title.id$>$title$</h$hdr.length$>");
+
+    /** emojis -> :emoji:
+     * list in emojis.js (https://github.com/KubaBoi/CheeseFramework/blob/webServices/mdConverter/emojis.js)
+     * credit https://github.com/privatenumber/gh-emojis
+     */
+    str = rplcReg(str, /:(?<emoji>[a-z0-9_\-\+]+):/g, "$emoji.emoji$");
+
+    var lines = str.split("\n");
+    str = "";
+    for (let i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        if (line.match(/^(\<)/) == null) {
+            if (i < lines.length-1) {
+                if (line == "" && lines[i+1] != "") {
+                    str += "<br><br>";
+                    continue;
+                }
+                else if (lines[i+1] == "") {
+                    str += `${line}<br><br>`
+                    i++;
+                    continue;
+                }
+            }
+            str += line;
+        }
+        else {
+            if (line.match(/\<pre.*/) != null) {
+                while (line.match(/\<\/pre\>/) == null) {
+                    str += line + "\n";
+                    line = lines[++i];
+                }
+            }
+            str += line;
+        }
+    }
+
+    mdDiv.innerHTML = str;
+}
+
+//console.log("http://github.com/KubaBoi/CheeseFramework/tree/development</code>".match(/(https*\:\/\/.* *)(\<\/code\>)$/g));
