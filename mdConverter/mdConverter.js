293f8da89f327073230c21812c34d97e22d36c5f
KubaBoi <jakubanderle@outlook.cz>
2022-05-11 20:58:26 +0200

    debug

--- mdConverter/mdConverter.js
+++ mdConverter/mdConverter.js
@@ -18,7 +18,7 @@ function convert(str) {
     
     // one line codes -> ```code```
     // no space
-    str = rplcReg(str, /\`{3}(?<code>[a-zA-Z0-9\#\@\&\?\/\:\=\"\']+)\`{3}/g, "<code class=''>$code$</code>");
+    str = rplcReg(str, /\`{3}(?<code>[a-zA-Z0-9\#\@\&\?\/\:\=\"\'\(\)\.\,\*\[\]]+)\`{3}/g, "<code class=''>$code$</code>");
     // with space
     str = rplcReg(str, /\`{3}(?<code>.+)\`{3}/g, "<code>$code$</code>");
     
@@ -41,14 +41,7 @@ function convert(str) {
 
     str = mdDiv.innerHTML;
 
-    // headers -> ## Header
-    str = rplcReg(str, /((?<!\>)(?<hdr>#+)) (?<title>.*)/g, "<h$hdr.length$ id=$title.id$>$title$</h$hdr.length$>");
-
-    /** emojis -> :emoji:
-     * list in emojis.js (https://github.com/KubaBoi/CheeseFramework/blob/webServices/mdConverter/emojis.js)
-     * credit https://github.com/privatenumber/gh-emojis
-     */
-    str = rplcReg(str, /:(?<emoji>[a-z0-9_\-\+]+):/g, "$emoji.emoji$");
+    str = headers(str);
 
     var lines = str.split("\n");
     str = "";
@@ -82,4 +75,37 @@ function convert(str) {
     mdDiv.innerHTML = str;
 }
 
+function headers(str) {
+    var lines = str.split("\n");
+
+    var newStr = "";
+    var paragraph = "";
+    var parId = "";
+
+    for (let i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        if (line.startsWith("#")) {
+            if (paragraph != "") {
+                newStr += `<div id=${parId}>${paragraph}</div>`;
+                paragraph = "";
+            }
+            parId = rplcReg(line, /((?<!\>)(?<hdr>#+)) (?<title>.*)/g, "$title.id$");
+            line = rplcReg(line, /((?<!\>)(?<hdr>#+)) (?<title>.*)/g, "<h$hdr.length$ id=$title.id$>$title$</h$hdr.length$>");
+        }
+        paragraph += line + "\n";
+    }
+    newStr += `<div id=${parId}>${paragraph}</div>`;
+    newStr = rplcReg(newStr, /:(?<emoji>[a-z0-9_\-\+]+):/g, "$emoji.emoji$");
+    return newStr;
+}
+
+/*
+// headers -> ## Header
+    str = rplcReg(str, /((?<!\>)(?<hdr>#+)) (?<title>.*)/g, "<h$hdr.length$ id=$title.id$>$title$</h$hdr.length$>");
+
+    /** emojis -> :emoji:
+     * list in emojis.js (https://github.com/KubaBoi/CheeseFramework/blob/webServices/mdConverter/emojis.js)
+     * credit https://github.com/privatenumber/gh-emojis
+     
+     str = rplcReg(str, /:(?<emoji>[a-z0-9_\-\+]+):/g, "$emoji.emoji$");*/
 //console.log("http://github.com/KubaBoi/CheeseFramework/tree/development</code>".match(/(https*\:\/\/.* *)(\<\/code\>)$/g));
