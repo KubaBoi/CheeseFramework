7c46d5bbd10123f3b3b10da2d7cf84e7ff325fac
Jakub Anderle <jakub.anderle@aimtecglobal.com>
2022-05-12 08:44:30 +0200

    test build

--- cheeserScripts/builders/projectBuilder.py
+++ cheeserScripts/builders/projectBuilder.py
@@ -65,7 +65,9 @@ class ProjectBuilder:
         queries = 0
         commits = 0
         for key in data.keys():
-            if (key == "SECRETS"): continue
+            if (key == "SECRETS" or
+                key == "ADMIN" or 
+                key == "SECURITY"): continue
             for pKey in data[key].keys():
                 cont = data[key][pKey]
                 for methodKey in cont["METHODS"].keys():
