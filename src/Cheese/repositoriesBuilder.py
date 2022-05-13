c2d6d8a96baf645e7b825569a567d7c317ab096c
GitHub Action <action@github.com>
2022-05-13 19:55:55 +0000

    Test build - 22.05.13.21.55 v(1.3.17)

--- src/Cheese/repositoriesBuilder.py
+++ src/Cheese/repositoriesBuilder.py
@@ -24,6 +24,11 @@ class RepositoriesBuilder:
             ["REPOSITORY", "DBSCHEME", "DBMODEL"],
             [("QUERY", [("RETURN", "raw")]), ("COMMIT", [])])
 
+        for repo in parent.dictJson["REPOSITORIES"].keys():
+            scheme = parent.dictJson["REPOSITORIES"][repo]["DBSCHEME"]
+            pk = re.search("\((?P<pk>\w+),.*", scheme)
+            parent.dictJson["PRIMARY_KEY"] = pk["pk"]
+
         RepositoriesBuilder.preQueries(parent)
         RepositoriesBuilder.preCommits(parent)
     
@@ -34,6 +39,18 @@ class RepositoriesBuilder:
         for repoKey in parent.dictJson["REPOSITORIES"].keys():
             repo = parent.dictJson["REPOSITORIES"][repoKey]
 
+            repo["METHODS"]["findAll"] = {
+                "QUERY": f"select * from {repo['REPOSITORY']};",
+                "RETURN": "array"
+            }
+            repo["METHODS"]["find"] = {
+                "QUERY": f"select * from {repo['REPOSITORY']} where {repo['PRIMARY_KEY']}=:primaryKey;",
+                "RETURN": "one"
+            }
+            repo["METHODS"]["findBy"] = {
+                "QUERY": f"select * from {repo['REPOSITORY']} where :columnName=:value;",
+                "RETURN": "array"
+            }
             repo["METHODS"]["findNewId"] = {
                     "QUERY": f"select max(id) from {repo['REPOSITORY']};",
                     "RETURN": "num"
@@ -53,11 +70,11 @@ class RepositoriesBuilder:
                 }
             
             repo["METHODS"]["update"] = {
-                    "COMMIT": f"update {name} set {scheme} = :obj where id=obj.id;",
+                    "COMMIT": f"update {name} set {scheme} = :obj where {repo['PRIMARY_KEY']}=:obj.{repo['PRIMARY_KEY']};",
                     "RETURN": ""
                 }
 
             repo["METHODS"]["delete"] = {
-                    "COMMIT": f"delete from {name} where id=model.id;",
+                    "COMMIT": f"delete from {name} where {repo['PRIMARY_KEY']}=:model.{repo['PRIMARY_KEY']};",
                     "RETURN": ""
                 }
--- src/Cheese/variables.py
+++ src/Cheese/variables.py
@@ -1,5 +1,5 @@
 #cheese
 
 class Variables:
-    release = "1.3.16"
+    release = "1.3.17"
     documentation = "https://kubaboi.github.io/CheeseFramework/"
