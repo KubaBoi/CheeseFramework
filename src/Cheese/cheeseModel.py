ea184168e8298ebff960b0a1bd83a6e0cea18af9
GitHub Action <action@github.com>
2022-06-13 14:34:14 +0000

    Test build - 22.06.13.16.33 v(1.3.86)

--- src/Cheese/cheeseModel.py
+++ src/Cheese/cheeseModel.py
@@ -1,18 +1,54 @@
 #cheese
 
+import inspect
+
 class CheeseModel:
+    """
+    `CheeseModel` is non-static class for storing data from database.
+    """
     
     def __init__(self, modelName, scheme):
+        """
+        Model documentation:
+        https://kubaboi.github.io/CheeseFramework/#73-models
+
+        `modelName` name of model for better orientation. 
+        It can be changed with `#@dbmodel ;` annotation.
+
+        `scheme` database scheme as list of strings. 
+        It can be changed with `#@dbscheme ;` annotation.
+        """
         self.modelName = modelName
         self.scheme = scheme
 
-    def toJson(self):
+    def toJson(self, allData=True) -> dict:
+        """
+        return model data as dictionary
+
+        `allData` 
+        - if `True` than in returned json will be 
+        every attribute of object except for `modelName` and `scheme`
+        - if `False` than in returned json will be 
+        only attributes from `scheme`
+        """
+        scheme = self.scheme
+        if (allData):
+            scheme = self.__getAttrs()
+
         jsn = {}
-        for attr in self.scheme:
+        for attr in scheme:
+            if (attr == "modelName" or attr == "scheme"): continue
             jsn[attr.upper()] = getattr(self, attr)
         return jsn
 
-    def toModel(self, jsn):
+    def toModel(self, jsn) -> None:
+        """
+        converts `dict` or anything iterable into `model`
+
+        `jsn` is an object with data
+        - if it is NOT `dict` than it need to be iterable and 
+        it needs to be in same order as `scheme` is. (tuple, list...)
+        """
         if (type(jsn).__name__ == "dict"):
             for attr in self.scheme:
                 if (attr.upper() == "ID"): continue
@@ -26,5 +62,19 @@ class CheeseModel:
             for attr, value in zip(self.scheme, jsn):
                 setattr(self, attr, value)
 
-    def setAttrs(self, **attrs):
-        return self.toModel(attrs)
+    def setAttrs(self, **attrs) -> None:
+        """
+        converts `kwargs` into model such as `toModel()` method
+
+        `**attrs` is an kwargs object. 
+        It will be passed to `toModel()` method as `dict`.
+        """
+        self.toModel(attrs)
+
+    def __getAttrs(self):
+        """
+        returns every attribute in `self` object
+        """
+        attributes = inspect.getmembers(self, lambda a:not(inspect.isroutine(a)))
+        attributes = [a[0] for a in attributes if not(a[0].startswith('__') and a[0].endswith('__'))]
+        return attributes
--- src/Cheese/variables.py
+++ src/Cheese/variables.py
@@ -1,5 +1,5 @@
 #cheese
 
 class Variables:
-    release = "1.3.85"
+    release = "1.3.86"
     documentation = "https://kubaboi.github.io/CheeseFramework/"
